6d4096c3db55503748e8ca33828508053c5a521e
Dmitry Kostjuchenko <kdp.029029@gmail.com>
2024-07-16 13:11:35 +0300

    добавил запуск в селеноиде

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -19,7 +19,7 @@ public class BaseTest {
         Configuration.browserSize = "1400x1050";
         Configuration.pageLoadStrategy = "eager";
         //SelenideLogger.addListener("allure", new AllureSelenide());
-        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         /*DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
