edb1c2752fd314284b3e7b223bdd3f9e8558a06b
Dmitry Kostjuchenko <kdp.029029@gmail.com>
2024-07-16 12:51:25 +0300

    изменил размер экрана

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -16,7 +16,7 @@ public class BaseTest {
     @BeforeAll
     static void setup() {
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = "1400x1050";
         Configuration.pageLoadStrategy = "eager";
         //SelenideLogger.addListener("allure", new AllureSelenide());
         //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
